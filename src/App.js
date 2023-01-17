8af2fb845c3dc8dd28dda9225be2f35dff83678d
DeloneCode <immigrante_annonimo@hotmail.com>
2023-01-17 12:32:20 -0500

    k

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import styled from 'styled-components'
 import MoonIcon from "./components/icons/MoonIcon";
 import SunIcon from "./components/icons/SunIcon";
 import Switch from "./components/Switch";
 
-const style = {
-  minHeight: "100vh",
-  textAlign: "center",
-  paddingTop: "10rem",
+
+const StyledApp = styled.div`
+  min-Height: "100vh",
+  text-Align: "center",
+  padding-Top: "10rem",
   backgroundColor: "#f5f5f5",
-};
+`;
 
 function App() {
   return (
-    <div style={style}>
+    <StyledApp >
       <SunIcon />
       <Switch />
       <MoonIcon />
-      <h1>Jim Naysium</h1>
-      <p>Username: jimnays10</p>
-      <p>Email: jim@gmail.com</p>
-    </div>
+      <h1>Code Delone</h1>
+      <p>Email: delonecode@gmail.com</p>
+    </StyledApp>
   );
 }
 
